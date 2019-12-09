a4ad810801bd009180e4b9db19cf0e23e279f5f7
Robin Stocker <robin@nibor.org>
2019-12-09 17:12:43 +1100

    Day 9
    Took a while until I figured out that "relative base" also has to be
    used for storing values (target position).

---
+++ src/bin/day09.rs
@@ -0,0 +1,11 @@
+//! https://adventofcode.com/2019/day/8
+
+use advent_of_code_2019::Intcode;
+
+fn main() {
+    let input = include_str!("../../input/2019/day09.txt").trim();
+    let code = Intcode::parse(input);
+
+    println!("Part 1: {:?}", code.clone().add_input(1).run_all());
+    println!("Part 2: {:?}", code.clone().add_input(2).run_all());
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,15 @@ use std::collections::LinkedList;
 
 #[derive(Clone)]
 pub struct Intcode {
-    prog: Vec<i32>,
-    ip: i32,
-    inputs: LinkedList<i32>,
+    prog: Vec<i128>,
+    ip: i128,
+    inputs: LinkedList<i128>,
+    relative_base: i128,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Result {
-    Output(i32),
+    Output(i128),
     Halt,
 }
 
@@ -26,20 +27,21 @@ impl Intcode {
         Self::new(prog)
     }
 
-    pub fn new(prog: Vec<i32>) -> Self {
+    pub fn new(prog: Vec<i128>) -> Self {
         Self {
             prog,
             ip: 0,
             inputs: LinkedList::new(),
+            relative_base: 0,
         }
     }
 
-    pub fn add_input(&mut self, input: i32) -> &mut Self {
+    pub fn add_input(&mut self, input: i128) -> &mut Self {
         self.inputs.push_back(input);
         self
     }
 
-    pub fn run_until_halt(&mut self) -> i32 {
+    pub fn run_last(&mut self) -> i128 {
         let mut output = 0;
         while let Result::Output(o) = self.run() {
             output = o;
@@ -47,37 +49,44 @@ impl Intcode {
         output
     }
 
+    pub fn run_all(&mut self) -> Vec<i128> {
+        let mut outputs = Vec::new();
+        while let Result::Output(o) = self.run() {
+            outputs.push(o);
+        }
+        outputs
+    }
+
     pub fn run(&mut self) -> Result {
         loop {
             let instruction = self.get(self.ip);
             let op = instruction % 100;
             let mut modes = instruction / 100;
-            let mode_a = modes % 10;
+            let mode1 = modes % 10;
             modes /= 10;
-            let mode_b = modes % 10;
+            let mode2 = modes % 10;
+            modes /= 10;
+            let mode3 = modes % 10;
 
             match op {
                 99 => break,
                 // TODO: Abstract over instructions, so that we can maybe inspect/modify programs too?
                 1 => {
-                    let a = self.param(self.ip + 1, mode_a);
-                    let b = self.param(self.ip + 2, mode_b);
-                    let t = self.get(self.ip + 3);
-                    self.set(t, a + b);
+                    let a = self.param(self.ip + 1, mode1);
+                    let b = self.param(self.ip + 2, mode2);
+                    self.store(self.ip + 3, mode3, a + b);
                     self.ip += 4;
                 }
                 2 => {
-                    let a = self.param(self.ip + 1, mode_a);
-                    let b = self.param(self.ip + 2, mode_b);
-                    let t = self.get(self.ip + 3);
-                    self.set(t, a * b);
+                    let a = self.param(self.ip + 1, mode1);
+                    let b = self.param(self.ip + 2, mode2);
+                    self.store(self.ip + 3, mode3, a * b);
                     self.ip += 4;
                 }
                 3 => {
                     // input
-                    let t = self.get(self.ip + 1);
                     if let Some(input) = self.inputs.pop_front() {
-                        self.set(t, input);
+                        self.store(self.ip + 1, mode1, input);
                     } else {
                         panic!("Expected input but didn't have any");
                     }
@@ -85,14 +94,14 @@ impl Intcode {
                 }
                 4 => {
                     // output
-                    let a = self.param(self.ip + 1, mode_a);
+                    let a = self.param(self.ip + 1, mode1);
                     self.ip += 2;
                     return Result::Output(a);
                 }
                 5 => {
                     // jump-if-true
-                    let a = self.param(self.ip + 1, mode_a);
-                    let b = self.param(self.ip + 2, mode_b);
+                    let a = self.param(self.ip + 1, mode1);
+                    let b = self.param(self.ip + 2, mode2);
                     if a != 0 {
                         self.ip = b;
                     } else {
@@ -101,8 +110,8 @@ impl Intcode {
                 }
                 6 => {
                     // jump-if-false
-                    let a = self.param(self.ip + 1, mode_a);
-                    let b = self.param(self.ip + 2, mode_b);
+                    let a = self.param(self.ip + 1, mode1);
+                    let b = self.param(self.ip + 2, mode2);
                     if a == 0 {
                         self.ip = b;
                     } else {
@@ -111,27 +120,31 @@ impl Intcode {
                 }
                 7 => {
                     // less than
-                    let a = self.param(self.ip + 1, mode_a);
-                    let b = self.param(self.ip + 2, mode_b);
-                    let t = self.get(self.ip + 3);
+                    let a = self.param(self.ip + 1, mode1);
+                    let b = self.param(self.ip + 2, mode2);
                     if a < b {
-                        self.set(t, 1);
+                        self.store(self.ip + 3, mode3, 1);
                     } else {
-                        self.set(t, 0);
+                        self.store(self.ip + 3, mode3, 0);
                     }
                     self.ip += 4;
                 }
                 8 => {
                     // equals
-                    let a = self.param(self.ip + 1, mode_a);
-                    let b = self.param(self.ip + 2, mode_b);
-                    let t = self.get(self.ip + 3);
+                    let a = self.param(self.ip + 1, mode1);
+                    let b = self.param(self.ip + 2, mode2);
                     if a == b {
-                        self.set(t, 1);
+                        self.store(self.ip + 3, mode3, 1);
                     } else {
-                        self.set(t, 0);
+                        self.store(self.ip + 3, mode3, 0);
                     }
                     self.ip += 4;
+                }
+                9 => {
+                    // adjusts the relative base
+                    let a = self.param(self.ip + 1, mode1);
+                    self.relative_base += a;
+                    self.ip += 2;
                 }
                 _ => unimplemented!("Unknown opcode {}", op),
             }
@@ -139,20 +152,42 @@ impl Intcode {
         Result::Halt
     }
 
-    fn get(&self, p: i32) -> i32 {
+    fn get(&mut self, p: i128) -> i128 {
+        self.ensure_memory(p);
         self.prog[p as usize]
     }
 
-    fn set(&mut self, p: i32, value: i32) {
+    fn set(&mut self, p: i128, value: i128) {
+        self.ensure_memory(p);
         self.prog[p as usize] = value;
     }
 
-    fn param(&self, p: i32, mode: i32) -> i32 {
+    fn ensure_memory(&mut self, p: i128) {
+        let needed_size = p as usize + 1;
+        if needed_size > self.prog.len() {
+            self.prog.resize(needed_size, 0);
+        }
+    }
+
+    fn param(&mut self, p: i128, mode: i128) -> i128 {
         let val = self.get(p);
-        if mode == 0 {
-            self.get(val)
-        } else {
-            val
+        match mode {
+            // position
+            0 => self.get(val),
+            // immediate
+            1 => val,
+            // relative
+            2 => self.get(self.relative_base + val),
+            _ => panic!("Unknown mode {} for param", mode),
+        }
+    }
+
+    fn store(&mut self, p: i128, mode: i128, result: i128) {
+        let val = self.get(p);
+        match mode {
+            0 => self.set(val, result),
+            2 => self.set(self.relative_base + val, result),
+            _ => panic!("Unknown mode {} for store", mode),
         }
     }
 }
@@ -175,7 +210,22 @@ mod tests {
         let input = include_str!("../input/2019/day05.txt");
         let code = Intcode::parse(input);
 
-        assert_eq!(code.clone().add_input(1).run_until_halt(), 5346030);
-        assert_eq!(code.clone().add_input(5).run_until_halt(), 513116);
+        assert_eq!(code.clone().add_input(1).run_last(), 5346030);
+        assert_eq!(code.clone().add_input(5).run_last(), 513116);
+    }
+
+    #[test]
+    fn test_day9_examples() {
+        let code = Intcode::parse("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99");
+        assert_eq!(
+            code.clone().run_all(),
+            vec![109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
+        );
+
+        let code = Intcode::parse("1102,34915192,34915192,7,4,7,99,0");
+        assert_eq!(code.clone().run_last(), 1219070632396864);
+
+        let code = Intcode::parse("104,1125899906842624,99");
+        assert_eq!(code.clone().run_last(), 1125899906842624);
     }
 }
